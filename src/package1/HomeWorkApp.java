51a5e73ba7acb978bdd9e8d9d5fb6eac5c63fa25
peterwosne <pwosne@mail.ru>
2022-03-25 17:50:54 +0300

    Initial commit

---
+++ src/package1/HomeWorkApp.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+	// write your code here
+        
+    }
+}
---
+++ src/ru/geekbrains/lesson_2/HomeWorkApp.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson_2;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
